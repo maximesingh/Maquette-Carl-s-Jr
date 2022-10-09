ed47616013f1e9c386378fe940449d844e2f7847
Max <86778428+aznmax@users.noreply.github.com>
2022-10-09 14:42:36 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const items = document.querySelectorAll("img");
+const nbSlide = items.length;
+const suivant = document.querySelector(".nextArrow");
+const precedent = document.querySelector(".previousArrow");
+let count = 0;
+let time = 8000;
+
+function slideSuivante() {
+  items[count].classList.remove("active");
+
+  if (count < 3) {
+    count++;
+  } else {
+    count = 1;
+  }
+
+  items[count].classList.add("active");
+  setTimeout(slideSuivante, time)
+}
+window.onload = slideSuivante
+
+suivant.addEventListener("click", slideSuivante);
+
+
+
+function slidePrecedente() {
+  items[count].classList.remove("active");
+
+  if (count > 1) {
+    count--;
+  } else {
+    count = 3;
+  }
+
+  items[count].classList.add("active");
+}
+
+precedent.addEventListener("click", slidePrecedente);
