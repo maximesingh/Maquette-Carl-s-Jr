b22857ceed4b930961fdd98217a86a3f7af0508a
Max <86778428+aznmax@users.noreply.github.com>
2022-10-10 15:57:42 +0200

    upload slider

--- app.js
+++ app.js
@@ -3,21 +3,19 @@ const nbSlide = items.length;
 const suivant = document.querySelector(".nextArrow");
 const precedent = document.querySelector(".previousArrow");
 let count = 0;
-let time = 8000;
+let time = 10000;
 
 function slideSuivante() {
   items[count].classList.remove("active");
-
-  if (count < 3) {
+  if (count < 4) {
     count++;
   } else {
-    count = 1;
+   count = 2
   }
-
   items[count].classList.add("active");
-  setTimeout(slideSuivante, time)
+  // setTimeout(slideSuivante, time)
 }
-window.onload = slideSuivante
+// window.onload = slideSuivante
 
 suivant.addEventListener("click", slideSuivante);
 
@@ -26,10 +24,10 @@ suivant.addEventListener("click", slideSuivante);
 function slidePrecedente() {
   items[count].classList.remove("active");
 
-  if (count > 1) {
-    count--;
+  if (count > 2) {
+    count--
   } else {
-    count = 3;
+    count = 4;
   }
 
   items[count].classList.add("active");
@@ -39,11 +37,11 @@ precedent.addEventListener("click", slidePrecedente);
 
 // Menu Hamburger Toggle //
 
-const btnMenu = document.querySelector('.toggleMenu')
-const menuList= document.querySelector('.linkNavigation')
+// const btnMenu = document.querySelector('.toggleMenu')
+// const menuList= document.querySelector('.linkNavigation')
 
-btnMenu.addEventListener('click',function(){
+// btnMenu.addEventListener('click',function(){
 
-menuList.classList.toggle('linkNavigation')
+// menuList.classList.toggle('linkNavigation')
 
-})
+// })
